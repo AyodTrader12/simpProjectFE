d8a255d76d90056b1141dcca6235c4128cffd294
Ayodtrader12 <ibrahimpopoola292@gmail.com>
2025-07-17 13:50:22 +0100

    new

--- src/pages/SCREEN/landingPage.tsx
+++ src/pages/SCREEN/landingPage.tsx
@@ -8,7 +8,7 @@ const landingPage = () => {
       <div className='w-full h-screen bg-[#1A1A1A]'>
         <h1 className='text-white text-4xl text-center pt-20'>Welcome to the Landing Page</h1>
         <p className='text-white text-center mt-4'>This is the starting point of our application.</p>
-        
+        <p className="text-white mt-4 text-center">This project is a basic authentication system where users can signup,login,verify account and reset password securely.</p>
         <div className='flex justify-center mt-10'>
           <Link to={"/auth/register"}>
           <button className='bg-blue-600 text-white px-6 py-2 rounded-xl hover:bg-blue-700 transition cursor-pointer'>
